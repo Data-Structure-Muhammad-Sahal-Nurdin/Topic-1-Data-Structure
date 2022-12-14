2f0ad9d4a32d731293469474d85948d77e341056
hp <sahalnurdin@gmail.com>
2022-12-14 07:48:30 +0700

    Add some files!!!

--- SORTED.py
+++ SORTED.py
@@ -2,14 +2,5 @@
 # a = ['SANTI', 'ANI', 'BUDI', 'RUDI', 'KARIN', 'DITA']
 # print(sorted(a))
 
-my_input = [1, 2, 3, 4, 5]
-
-print(f'Current Numbers List {my_input}')
-
-number = int(input("Please enter a number to be added:\n"))
-
-index = int(input(f'Enter the index between 0 and {len(my_input) - 1} to add the given number:\n'))
-
-my_input.insert(index, number)
-
-print(f'Updated List {my_input}')
+x = [x for x in input("Masukkan data: ").split()]
+print("URUTAD DATA YANG SUDAH DI SORTED: ", sorted(x))
