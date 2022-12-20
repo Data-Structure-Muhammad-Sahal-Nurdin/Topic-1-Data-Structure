3dbeffb9444a0438dd29089cf2e02cc110b0cd67
hp <sahalnurdin@gmail.com>
2022-12-20 18:33:33 +0700

    Add some files!!!

--- SORTED.py
+++ SORTED.py
@@ -2,7 +2,8 @@
 # a = ['SANTI', 'ANI', 'BUDI', 'RUDI', 'KARIN', 'DITA']
 # print(sorted(a))
 
-x = [x for x in input("Masukkan data: ").split()]
+# x = [x for x in input("Masukkan data: ").split()]
+# print("URUTAD DATA YANG SUDAH DI SORTED: ", sorted(x))
+
+x = [int(x) for x in input("Masukkan data: ").split()]
 print("URUTAD DATA YANG SUDAH DI SORTED: ", sorted(x))
-
-
